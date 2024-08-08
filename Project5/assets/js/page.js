14d6b984df1d9a8920fed42d190e9b0faea95e00
Szpilusia <szpilka112@gmail.com>
2024-08-08 21:15:20 +0200

    Add files via upload

---
+++ Project5/assets/js/page.js
@@ -0,0 +1,45 @@
+/*============================================
+    dropdown menu
+============================================*/
+
+
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+/*============================================
+    change tabs
+============================================*/
+
+
+
+function changeTab(evt, tabNum) { 
+    
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+   tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabNum).style.display = "block";
+    evt.currentTarget.className += " active";
+}
